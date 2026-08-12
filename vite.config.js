f13c32f38526e49808591bcd447fe5923b6e8398
Karthik A <147269422+KARTHIK-A4@users.noreply.github.com>
2026-08-12 19:18:59 +0530

    Change base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/myapp/",
+  base: 'https://karthik-a4.github.io/myapp.io/',
   plugins: [react()],
 })
